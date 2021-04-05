a41ea2543dc18d20b13b00518142064ed57b5b05
farkasnoemi1712 <farkasnoemi1712@gmail.com>
2021-04-05 10:56:25 +0300

    Initial commit

---
+++ Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+        private readonly IRestaurantData restaurantData;
+
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null) {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
